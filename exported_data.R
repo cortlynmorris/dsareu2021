32b13fdeaaae1a359847cf2b5146c4c44c6ff895
cortlynmorris <85583715+cortlynmorris@users.noreply.github.com>
2021-06-13 11:04:01 -0400

    Add files via upload

---
+++ exported_data.R
@@ -0,0 +1,52 @@
+library(reshape2)
+library(tidyverse)
+setwd("/Users/cmorris/Desktop/dsreu2021/rstudiodirectory/ResearchProject")
+
+persons <- read.csv(file="Persons_Involved_in_Crashes.csv")
+glimpse(persons)
+
+locations <- read.csv(file="Reported_Crash_Locations.csv")
+glimpse(locations)
+
+crashes <- persons %>% left_join(locations, by="key_crash")
+
+library(ggplot2)
+library(dplyr)
+library(dslabs)
+library(ggrepel)
+library(ggthemes)
+
+## After log transformation
+crashes %>% 
+  filter(VehicleType != "Unknown", VehicleType != "") %>%
+  count(VehicleType) %>% 
+  mutate(logtrans = round(log10(n), digits = 2), 
+         VehicleType = reorder(VehicleType,logtrans)) %>% 
+  ggplot(aes(x=VehicleType, y=logtrans, fill=VehicleType)) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  coord_flip() + 
+  geom_text(aes(label=logtrans),nudge_y=0.5) 
+
+crashes %>% 
+  count(WeatherCondition1) %>% 
+  mutate(logtrans = round(log10(n), digits = 2)) %>% 
+  ggplot(aes(x=WeatherCondition1, y=logtrans, fill=WeatherCondition1)) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  coord_flip() + 
+  geom_text(aes(label=logtrans),nudge_y=0.5)
+
+crashes %>% 
+  count(TrafficControlType) %>% 
+  mutate(logtrans = round(log10(n), digits = 2)) %>% 
+  ggplot(aes(x=TrafficControlType, y=logtrans, fill=TrafficControlType)) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  coord_flip() + 
+  geom_text(aes(label=logtrans),nudge_y=0.5)
+
+crashes %>% 
+  count(RoadFeature) %>% 
+  mutate(logtrans = round(log10(n), digits = 2)) %>% 
+  ggplot(aes(x=RoadFeature, y=logtrans, fill=RoadFeature)) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  coord_flip() + 
+  geom_text(aes(label=logtrans),nudge_y=0.5)
