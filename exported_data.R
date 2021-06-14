a64ead974bc8db835944fa2b0619a69935e8c7c2
cortlynmorris <cmorris1@haverford.edu>
2021-06-14 12:15:20 -0400

    added more graphs of different variables

--- exported_data.R
+++ exported_data.R
@@ -43,6 +43,19 @@ crashes %>%
        y = "Count (log10 Scale)")
 
 crashes %>% 
+  filter(WeatherCondition1 != "NA", WeatherContributedToCrash == "Yes") %>%
+  count(WeatherCondition1) %>% 
+  mutate(logtrans = round(log10(n), digits = 2),
+         WeatherCondition1 = reorder(WeatherCondition1,logtrans)) %>% 
+  ggplot(aes(x=WeatherCondition1, y=logtrans, fill=WeatherCondition1)) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  coord_flip() + 
+  geom_text(aes(label=logtrans),nudge_y=0.5) +
+  labs(title = "Frequency of Crashes by Weather Condition",
+       x = "Weather Condition",
+       y = "Count (log10 Scale)")
+
+crashes %>% 
   filter(TrafficControlType != "NA", TrafficControlType != "", 
          TrafficControlType != "NaN") %>%
   count(TrafficControlType) %>% 
@@ -75,3 +88,49 @@ crashes %>%
   geom_histogram(aes(x=Age), binwidth = 5, col="red", fill="darkgrey") +
   labs(title="Histogram for Drivers' Ages", x="Age", y="Frequency")
 
+crashes %>% 
+  filter(AlcoholResultType != "NA", AlcoholResultType != "", 
+         AlcoholResultType != "NaN") %>%
+  count(AlcoholResultType) %>% 
+  mutate(logtrans = round(log10(n), digits = 2),
+         AlcoholResultType = reorder(AlcoholResultType,logtrans)) %>% 
+  ggplot(aes(x=AlcoholResultType, y=logtrans, fill=AlcoholResultType)) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  coord_flip() + 
+  geom_text(aes(label=logtrans),nudge_y=0.5) +
+  labs(title = "Frequency of Crashes by Alcohol Result Type",
+       x = "Alcohol Result Type",
+       y = "Count (log10 Scale)")
+# Question: Remove pending/unknown?
+
+crashes %>% 
+  filter(Crash_Date_DOW != "NA") %>%
+  mutate(Crash_Date_DOW = factor(Crash_Date_DOW, levels = c("Sunday","Monday", "Tuesday", "Wednesday", 
+                                   "Thursday", "Friday", "Saturday"))) %>%
+  ggplot() +
+  geom_bar(aes(Crash_Date_DOW, fill=Crash_Date_DOW), show.legend = FALSE) +
+  geom_text(stat="count", aes(x=Crash_Date_DOW, label=..count..), vjust=-0.25) +
+  labs(title = "Frequency of Crashes by Day of Week",
+       x = "Day of Week",
+       y = "Count")
+
+crashes %>% 
+  filter(Crash_Date_Month != "NA") %>%
+  mutate(Crash_Date_Month = factor(Crash_Date_Month, 
+                                   levels = c("January","February", "March", 
+                                              "April", "May", "June", "July",
+                                              "August", "September", "October",
+                                              "November", "December"))) %>%
+  ggplot() +
+  geom_bar(aes(Crash_Date_Month, fill=Crash_Date_Month), show.legend = FALSE) +
+  geom_text(stat="count", aes(x=Crash_Date_Month, label=..count..), vjust=-0.25) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(title = "Frequency of Crashes by Month",
+       x = "Month",
+       y = "Count")
+
+crashes %>%
+  filter(Crash_Date_Hour != "NA", Crash_Date_Hour != "") %>%
+  ggplot() +
+  geom_bar(aes(Crash_Date_Hour)) +
+  labs(title="Frequency of Crashes by Hour", x="Hour", y="Count")
